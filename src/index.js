c8b02f506515305d43d209ac5fb16bcd00eaf9de
Ilia <ilia@bookjane.com>
2019-02-15 09:33:08 -0500

    setting up basic app structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+const App = () => <div>app</div>
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
