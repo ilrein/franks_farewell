dfb9a2b6caf68b6cfddb87a3a0160d17fb7af634
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2019-03-07 23:13:46 -0500

    did not use company id as usestate value

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,6 +11,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import CompanyContainer from '../../containers/CompanyContainer';
+
 const Wrapper = styled.div`
   background-color: #eee;
   margin-top: 1rem;
@@ -24,93 +26,96 @@ const CoolCard = styled(Card)`
   }
 `;
 
-const Dashboard = ({ company }) => {
+const Dashboard = ({ userReducer, company }) => {
   // const [companyId] = useState(company._id);
-  const [name] = useState(company.name || '');
+  const { name } = company;
 
   return (
-    <Wrapper>
-      <Container>
-        <Header as="h1">
-          Dashboard
-        </Header>
-        <Grid
-          columns={4}
-          stackable
-        >
-          <Grid.Row>
-            <Grid.Column>
-              <Link to="/company">
-                <CoolCard
-                  header="Company"
-                  description={name ? `Update ${name}` : 'Configure company settings'}
-                />
-              </Link>
-            </Grid.Column>
+    <CompanyContainer>
+      <Wrapper>
+        <Container>
+          <Header as="h1">
+            Dashboard
+          </Header>
+          <Grid
+            columns={4}
+            stackable
+          >
+            <Grid.Row>
+              <Grid.Column>
+                <Link to="/company">
+                  <CoolCard
+                    header="Company"
+                    description={name ? `Update ${name}` : 'Configure company settings'}
+                  />
+                </Link>
+              </Grid.Column>
 
-            <Grid.Column>
-              <Link to="/locations">
+              <Grid.Column>
+                <Link to="/locations">
+                  <CoolCard>
+                    <Card.Content>
+                      <Card.Header>
+                        Locations
+                      </Card.Header>
+                      <Card.Description>
+                        Manage locations
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Icon name="building" />
+                      0 locations
+                    </Card.Content>
+                  </CoolCard>
+                </Link>
+              </Grid.Column>
+
+              <Grid.Column>
                 <CoolCard>
                   <Card.Content>
                     <Card.Header>
-                      Locations
+                      Shifts
                     </Card.Header>
                     <Card.Description>
-                      Manage locations
+                      Manage shifts
                     </Card.Description>
                   </Card.Content>
                   <Card.Content extra>
-                    <Icon name="building" />
-                    0 locations
+                    <Icon name="calendar" />
+                    0 shifts
                   </Card.Content>
                 </CoolCard>
-              </Link>
-            </Grid.Column>
+              </Grid.Column>
 
-            <Grid.Column>
-              <CoolCard>
-                <Card.Content>
-                  <Card.Header>
-                    Shifts
-                  </Card.Header>
-                  <Card.Description>
-                    Manage shifts
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <Icon name="calendar" />
-                  0 shifts
-                </Card.Content>
-              </CoolCard>
-            </Grid.Column>
-
-            <Grid.Column>
-              <CoolCard>
-                <Card.Content>
-                  <Card.Header>
-                    Staff
-                  </Card.Header>
-                  <Card.Description>
-                    Manage staff
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <Icon name="user" />
-                  0 users
-                </Card.Content>
-              </CoolCard>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Wrapper>
+              <Grid.Column>
+                <CoolCard>
+                  <Card.Content>
+                    <Card.Header>
+                      Staff
+                    </Card.Header>
+                    <Card.Description>
+                      Manage staff
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Icon name="user" />
+                    0 users
+                  </Card.Content>
+                </CoolCard>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Wrapper>
+    </CompanyContainer>
   );
 };
 
 Dashboard.propTypes = {
+  userReducer: PropTypes.shape().isRequired,
   company: PropTypes.shape().isRequired,
 };
 
 export default connect(
-  ({ company }) => ({ company }),
+  ({ userReducer, company }) => ({ userReducer, company }),
 )(Dashboard);
