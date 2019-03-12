c038d915113708e8279d43a6ef8a2fbfa796c26c
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2019-03-12 18:49:28 -0400

    improving dashbaord for specialists

--- src/pages/Dashboard/SpecialistView.js
+++ src/pages/Dashboard/SpecialistView.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  Header,
+} from 'semantic-ui-react';
 
 import SkillsetsContainer from '../../containers/SkillsetsContainer';
 
@@ -7,12 +12,32 @@ const Wrapper = styled.div`
   padding: 2rem;
 `;
 
-const SpecialistView = () => (
+/**
+ * Shows a list of possible positions
+ * if the current user has not specified one for themselves yet
+ */
+const SpecialistView = ({
+  userReducer,
+  skillsets,
+}) => (
   <SkillsetsContainer>
     <Wrapper>
-      non admin view
+      <Header>
+        {userReducer.user.email}
+      </Header>
     </Wrapper>
   </SkillsetsContainer>
 );
 
-export default SpecialistView;
+SpecialistView.propTypes = {
+  userReducer: PropTypes.shape().isRequired,
+  skillsets: PropTypes.shape().isRequired,
+};
+
+export default connect(({
+  userReducer,
+  skillsets,
+}) => ({
+  userReducer,
+  skillsets,
+}))(SpecialistView);
