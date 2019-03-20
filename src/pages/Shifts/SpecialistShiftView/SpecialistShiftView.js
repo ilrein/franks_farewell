52071b7a3b1db06ac71b2248c36cdea3a55e9c37
Ilia <ilia@bookjane.com>
2019-03-20 13:57:19 -0400

    specialistshiftview now pulls shifts based on specialistId

--- src/pages/Shifts/SpecialistShiftView/SpecialistShiftView.js
+++ src/pages/Shifts/SpecialistShiftView/SpecialistShiftView.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
+import getShiftsBySpecialistId from '../../../utils/requests/shifts/getShiftsBySpecialist';
 import Wrapper from '../../../components/Wrapper';
 
-const SpecialistShiftView = () => {
-  const getUserShifts = () => {
+const SpecialistShiftView = ({
+  user,
+  cognitoUser,
+}) => {
+  const [pulledJobs, setPulledJobs] = useState(false);
+  const [jwtToken] = useState(cognitoUser.signInUserSession.accessToken.jwtToken);
 
+  const getUserShifts = async (token, specialistId) => {
+    const list = await getShiftsBySpecialistId(token, specialistId);
+    console.log(list);
   };
-  
+
+  useEffect(() => {
+    getUserShifts(jwtToken, user._id);
+    setPulledJobs(true);
+  }, [pulledJobs]);
+
   return (
     <Wrapper>
       SpecialistShiftView
@@ -14,4 +28,9 @@ const SpecialistShiftView = () => {
   );
 };
 
-export default SpecialistShiftView;
+export default connect(
+  ({ userReducer }) => ({
+    user: userReducer.user,
+    cognitoUser: userReducer.cognitoUser,
+  }),
+)(SpecialistShiftView);
--- src/utils/requests/shifts/getShiftsBySpecialist.js
+++ src/utils/requests/shifts/getShiftsBySpecialist.js
@@ -4,8 +4,20 @@ import {
   API_GET_SHIFTS,
 } from '../../../constants';
 
-const getShiftsBySpecialist = () => {
-  //
+const getShiftsBySpecialist = async (jwtToken, specialistId) => {
+  try {
+    const get = await fetch(`${API_GET_SHIFTS}?specialistId=${specialistId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'jwt-token': jwtToken,
+      },
+    });
+
+    const result = await get.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 
 export default getShiftsBySpecialist;
